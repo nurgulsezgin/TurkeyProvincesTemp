50f70c95b4ee3526fb1e58d33d69a716ae829214
nurgül sezgin <nurgul_sezgin@hotmail.com>
2022-02-06 15:59:54 +0300

    Hava durumu bilgisi tüm iller için güncellendi.

--- TurkeyProvincesTemp/Program.cs
+++ TurkeyProvincesTemp/Program.cs
@@ -9,16 +9,20 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-
+string[] provincesList = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "turkiyedekiIller.txt")).Replace(" ","").Split(',');
+foreach (var province in provincesList)
+{
+    app.MapGet(string.Format("/tempature/city={0}", province), () => appMapGetModelJson(province));
+}
 RouteHandlerBuilder routeHandlerBuilder = app.MapGet("/", (Func<List<User>>)(() => new() { new("Nurgül", "Sezgin") }));
-app.MapGet("/tempature", () => GetModelJson()); 
+
 app.Run();
-static string GetModelJson()
+
+static string appMapGetModelJson(string cityName) 
 {
     // Create an HttpClient instance
     WebClient client = new WebClient();
-    string content = string.Format("http://api.openweathermap.org/data/2.5/weather?q=Cleveland&APPID=8f916d970abee3b68204508eeeb3695a&units=imperial");
+    string content = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0},tr&APPID=8f916d970abee3b68204508eeeb3695a&units=metric",cityName);
     //string json = File.ReadAllText("C:\\nurgul\\nurgul.json", Encoding.UTF8);
     string json = client.DownloadString(content);
     TurkeyProvincesTemp.Model.WeatherForecast.Root json2 = JsonConvert.DeserializeObject<TurkeyProvincesTemp.Model.WeatherForecast.Root>(json);
@@ -28,7 +32,6 @@ static string GetModelJson()
 
     return JsonConvert.SerializeObject(sýcaklýk);
 }
-
 public record User(string Name, string LastName)
 {
     public string FullName()
