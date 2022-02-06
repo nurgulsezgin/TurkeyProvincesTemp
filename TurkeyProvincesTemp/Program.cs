e96e3ac7019080152c8b167536cdf8f25b9ae744
nurgül sezgin <nurgul_sezgin@hotmail.com>
2022-02-06 12:51:10 +0300

    EndPointTest

--- TurkeyProvincesTemp/Program.cs
+++ TurkeyProvincesTemp/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Nurgül Sezgin");
+app.MapGet("/tempature", () => "Sýcaklýk");
 
 app.Run();
